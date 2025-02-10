9e6059dea07cc1ce26ce50b6d473274747b67f7f
UIDAM <jan635573@gmail.com>
2025-02-10 12:34:10 +0900

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="product-list">
+    <div class="card" v-for="list in products" :key="list.id">
+      <img :src="list.image" :alt="list.name"/>
+      <p>{{ list.name }}</p>
+      <p>가격 : {{ list.price }}원</p>
+      <button @click="addToList(list)">장바구니 추가</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    products: Array
+  });
+  const emit = defineEmits(['add-to-cart']);
+  const addToList = (list) => {
+    emit('add-to-cart', list);
+  }
+</script>
+
+<style lang="scss" scoped>
+  .product-list{
+    display: flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    justify-content: center;
+    .card{
+      width: 200px;
+      text-align: center;
+      img{
+        width: 100%;
+        height: 200px;
+        object-fit: cover;
+        border: 5px;
+      }
+    }
+  }
+</style>
